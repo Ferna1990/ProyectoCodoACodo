d9b638f5678d397bf9079bd2d4f3d17d19175aa7
Ferna1990 <fcrulo901312@gmail.com>
2023-11-28 18:04:08 -0300

    bueno, hice lo que pude, me faltan conocimientos mas de aca no puedo llegar...

--- app3.py
+++ app3.py
@@ -1,28 +1,166 @@
 
-from flask import Flask
-from flask import render_template
-from flaskext.mysql import MySQL
+# Importa las clases Flask, jsonify y request del módulo flask
+from flask import Flask, jsonify, request
+# Importa la clase CORS del módulo flask_cors
+from flask_cors import CORS
+# Importa la clase SQLAlchemy del módulo flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+# Importa la clase Marshmallow del módulo flask_marshmallow
+from flask_marshmallow import Marshmallow
 
-app= Flask(__name__)
-mysql=MySQL()
+
+# Crea una instancia de la clase Flask con el nombre de la aplicación
+app = Flask(__name__)
+# Configura CORS para permitir el acceso desde el frontend al backend
+CORS(app)
+
+# Configura la URI de la base de datos con el driver de MySQL, usuario, contraseña y nombre de la base de datos
+# URI de la BD == Driver de la BD://user:password@UrlBD/nombreBD
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@localhost/proyecto"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/productos"
+# Configura el seguimiento de modificaciones de SQLAlchemy a False para mejorar el rendimiento
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Crea una instancia de la clase SQLAlchemy y la asigna al objeto db para interactuar con la base de datos
+db = SQLAlchemy(app)
+# Crea una instancia de la clase Marshmallow y la asigna al objeto ma para trabajar con serialización y deserialización de datos
+ma = Marshmallow(app)
+
+class producto(db.Model):  # Producto hereda de db.Model
+    """
+    Definición de la tabla Producto en la base de datos.
+    La clase Producto hereda de db.Model.
+    Esta clase representa la tabla "Producto" en la base de datos.
+    """
+    Id = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(100))
+    Foto = db.Column(db.String(200))
+    Color = db.Column(db.String(30))
+    Calorias = db.Column(db.String(30))
+
+    def __init__(self, nombre, Foto, Color, Calorias):
+        """
+        Constructor de la clase Producto.
+
+        Args:
+            nombre (str): Nombre del producto.
+            Foto (str): URL o ruta de la imagen del producto.
+            Color (int): Color del valor calorico del producto.
+            Calorias (str): Nivel calorico del producto producto.
+        """
+        self.nombre = nombre
+        self.Foto = Foto
+        self.Color = Color
+        self.Calorias = Calorias
+
+    # Se pueden agregar más clases para definir otras tablas en la base de datos
+
+with app.app_context():
+    db.create_all()  # Crea todas las tablas en la base de datos
+
+# Definición del esquema para la clase Producto
+class ProductoSchema(ma.Schema):
+    """
+    Esquema de la clase Producto.
+
+    Este esquema define los campos que serán serializados/deserializados
+    para la clase Producto.
+    """
+    class Meta:
+        fields = ("id", "nombre", "Foto", "Color", "Calorias")
+
+producto_schema = ProductoSchema()  # Objeto para serializar/deserializar un producto
+productos_schema = ProductoSchema(many=True)  # Objeto para serializar/deserializar múltiples productos
+
+'''
+Este código define un endpoint que permite obtener todos los productos de la base de datos y los devuelve como un JSON en respuesta a una solicitud GET a la ruta /productos.
+@app.route("/productos", methods=["GET"]): Este decorador establece la ruta /productos para este endpoint y especifica que solo acepta solicitudes GET.
+def get_Productos(): Esta es la función asociada al endpoint. Se ejecuta cuando se realiza una solicitud GET a la ruta /productos.
+all_productos = Producto.query.all(): Se obtienen todos los registros de la tabla de productos mediante la consulta Producto.query.all(). Esto se realiza utilizando el modelo Producto que representa la tabla en la base de datos. El método query.all() heredado de db.Model se utiliza para obtener todos los registros de la tabla.
+result = productos_schema.dump(all_productos): Los registros obtenidos se serializan en formato JSON utilizando el método dump() del objeto productos_schema. El método dump() heredado de ma.Schema se utiliza para convertir los objetos Python en representaciones JSON.
+return jsonify(result): El resultado serializado en formato JSON se devuelve como respuesta al cliente utilizando la función jsonify() de Flask. Esta función envuelve el resultado en una respuesta HTTP con el encabezado Content-Type establecido como application/json.
+
+'''
+@app.route("/productos", methods=["GET"])
+def get_producto():
+    """
+    Endpoint para obtener todos los productos de la base de datos.
+
+    Retorna un JSON con todos los registros de la tabla de productos.
+    """
+    all_productos = producto.query.all()  # Obtiene todos los registros de la tabla de productos
+    result = productos_schema.dump(all_productos)  # Serializa los registros en formato JSON
+    return jsonify(result)  # Retorna el JSON de todos los registros de la tabla
+
+'''
+El código que sigue a continuación termina de resolver la API de gestión de productos, a continuación se destaca los principales detalles de cada endpoint, incluyendo su funcionalidad y el tipo de respuesta que se espera.
+Endpoints de la API de gestión de productos:
+get_producto(Id):
+    # Obtiene un producto específico de la base de datos
+    # Retorna un JSON con la información del producto correspondiente al ID proporcionado
+delete_producto(Id):
+    # Elimina un producto de la base de datos
+    # Retorna un JSON con el registro eliminado del producto correspondiente al ID proporcionado
+create_producto():
+    # Crea un nuevo producto en la base de datos
+    # Lee los datos proporcionados en formato JSON por el cliente y crea un nuevo registro de producto
+    # Retorna un JSON con el nuevo producto creado
+update_producto(Id):
+    # Actualiza un producto existente en la base de datos
+    # Lee los datos proporcionados en formato JSON por el cliente y actualiza el registro del producto con el ID especificado
+    # Retorna un JSON con el producto actualizado
+
+'''
+
+@app.route("/productos", methods=["DELETE"])
+def delete_producto(id):
+    """
+    Endpoint para eliminar un producto de la base de datos.
+
+    Elimina el producto correspondiente al ID proporcionado y retorna un JSON con el registro eliminado.
+    """
+    producto = producto.query.get(id)  # Obtiene el producto correspondiente al ID recibido
+    db.session.delete(producto)  # Elimina el producto de la sesión de la base de datos
+    db.session.commit()  # Guarda los cambios en la base de datos
+    return producto_schema.jsonify(producto)  # Retorna el JSON del producto eliminado
+
+@app.route("/productos", methods=["POST"])  # Endpoint para crear un producto
+def create_producto():
+    """
+    Endpoint para crear un nuevo producto en la base de datos.
+
+    Lee los datos proporcionados en formato JSON por el cliente y crea un nuevo registro de producto en la base de datos.
+    Retorna un JSON con el nuevo producto creado.
+    """
+    Nombre = request.json["nombre"]  # Obtiene el nombre del producto del JSON proporcionado
+    Foto = request.json["Foto"]  # Obtiene el precio del producto del JSON proporcionado
+    Color = request.json["Color"]  # Obtiene el stock del producto del JSON proporcionado
+    Calorias = request.json["Calorias"]  # Obtiene la imagen del producto del JSON proporcionado
+    new_producto = producto(Nombre, Foto, Color, Calorias)  # Crea un nuevo objeto Producto con los datos proporcionados
+    db.session.add(new_producto)  # Agrega el nuevo producto a la sesión de la base de datos
+    db.session.commit()  # Guarda los cambios en la base de datos
+    return producto_schema.jsonify(new_producto)  # Retorna el JSON del nuevo producto creado
+
+@app.route("/productos<id>", methods=["PUT"])  # Endpoint para actualizar un producto
+def update_producto(id):
+    """
+    Endpoint para actualizar un producto existente en la base de datos.
+
+    Lee los datos proporcionados en formato JSON por el cliente y actualiza el registro del producto con el ID especificado.
+    Retorna un JSON con el producto actualizado.
+    """
+    producto = producto.query.get(id)  # Obtiene el producto existente con el ID especificado
+
+    # Actualiza los atributos del producto con los datos proporcionados en el JSON
+    producto.Nombre = request.json["nombre"]
+    producto.Foto = request.json["Foto"]
+    producto.Color = request.json["Color"]
+    producto.Calorias = request.json["Calorias"]
+
+    db.session.commit()  # Guarda los cambios en la base de datos
+    return producto_schema.jsonify(producto)  # Retorna el JSON del producto actualizado
 
 
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-app.config['MYSQL_DATABASE_HOST'] = 'root'
-app.config['MYSQL_DATABASE_HOST'] = ''
-app.config['MYSQL_DATABASE_HOST'] = 'productos'
-
-mysql.init_app(app)
-
-@app.route('/')
-def index():
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    sql = "insert into producto(nombre) values (Pizza);"
-    cursor.execute(sql)
-    conn.commit()
-
-    return render_template('')
-
-if __name__== '__main__':
+# Programa Principal
+if __name__ == "__main__":
+    # Ejecuta el servidor Flask en el puerto 3306 en modo de depuración
     app.run(debug=True)
