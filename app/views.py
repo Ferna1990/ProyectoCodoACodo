2f984ec4454100c02680d572adb41ef2dbda716c
Ferna1990 <fcrulo901312@gmail.com>
2023-11-29 17:21:21 -0300

    Produje un JSON no lo puedo creer maldita seaaaaa!

--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from .models import producto
+from django.http.response import JsonResponse
+
+
 
 # Create your views here.
 
 
-def SemaforoCaloriasConJSVanilla(request):
-    return render(reguest, 'SemaforoCaloriasConJSVanilla.html')
+def index(request):
+    return render(request, 'SemaforoCaloriasConJSVanilla.html')
 
-
+def list_productos(request):
+    productos=list(producto.objects.values())
+    data= {'producto': productos}
+    return JsonResponse(data)    
 
 
 
