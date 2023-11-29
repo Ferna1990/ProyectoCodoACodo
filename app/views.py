ed26192b868daf5fffff9833cb4cb312ccee4546
Ferna1990 <fcrulo901312@gmail.com>
2023-11-29 15:24:41 -0300

    version 1.0.1.2 con django

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def SemaforoCaloriasConJSVanilla(request):
+    return render(reguest, 'SemaforoCaloriasConJSVanilla.html')
+
+
+
+
+
